470f79e311bb51a11d42b608fdb691d06b120522
Mohamed Khaled <80783747+M0Leo@users.noreply.github.com>
2023-10-24 04:31:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    let skills = ['HTML', 'CSS', 'JS', 'React', 'Node'];
+    let member = {
+        name: 'John',
+        age: 25,
+        skills: skills,
+    };
+    return member;
+}
